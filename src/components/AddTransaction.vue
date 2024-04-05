f84c6418abc111268955ac8498f0c3974a3954f3
Marcus Reis <mvsantosreis@gmail.com>
2024-04-05 15:00:51 +0100

    Add Transaction to State

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -27,6 +27,8 @@ import { useToast } from "vue-toastification";
 const text = ref("");
 const amount = ref("");
 
+const emit = defineEmits(["transactionSubmitted"]);
+
 const toast = useToast();
 
 const onSubmit = () => {
@@ -34,7 +36,13 @@ const onSubmit = () => {
     toast.error("Both fields must be filled");
     return;
   }
-  console.log(text.value, amount.value);
+
+  const transactionData = {
+    text: text.value,
+    amount: parseFloat(amount.value),
+  };
+  // console.log(text.value, amount.value);
+  emit("transactionSubmitted", transactionData);
 
   text.value = "";
   amount.value = "";
